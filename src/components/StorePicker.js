2d42334501df91592575b822461e1fee11e66989
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-22 05:25:25 +0200

    created an onSubmit event binder

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,9 +3,16 @@ import { render } from 'react-dom';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+   getStore (event) {
+      // stop the reloading
+      event.preventDefault();
+      console.log("Get Store Clicked!");
+    }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.getStore}>
         <h1>Select a Store</h1>
         <input type="text" name="storeName" placeholder="enter a store name" 
         defaultValue={getFunName()}
