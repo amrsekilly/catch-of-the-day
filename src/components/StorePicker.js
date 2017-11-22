7e7249c7266f6c7dd58cc46d4ec6c1dfefb3f609
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-22 05:49:08 +0200

    binded the method to the component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -7,12 +7,13 @@ class StorePicker extends React.Component {
    getStore (event) {
       // stop the reloading
       event.preventDefault();
-      console.log("Get Store Clicked!");
+      console.log("Get Store Clicked!: ", this);
+      
     }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={this.getStore}>
+      <form className="store-selector" onSubmit={(e) => this.getStore(e)}>
         <h1>Select a Store</h1>
         <input type="text" name="storeName" placeholder="enter a store name" 
         defaultValue={getFunName()}
