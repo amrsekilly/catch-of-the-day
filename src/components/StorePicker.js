18576892065b9096b44957fbfbddd0d3b4d87055
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-22 05:15:50 +0200

    invoked the getFunName helper in the form to generate store names

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h1>Select a Store</h1>
-        <input type="text" name="storeName" placeholder="enter a store name" required/>
+        <input type="text" name="storeName" placeholder="enter a store name" 
+        defaultValue={getFunName()}
+        required/>
         <button type="submit">Visit Store</button>
       </form>
     )
