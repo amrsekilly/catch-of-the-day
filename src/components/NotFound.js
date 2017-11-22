d7c10b557a1c5dc4aba2e7869f2c1db0aec0250f
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-22 05:08:47 +0200

    added the not found view

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <form className="store-selector">
+        <h1>404 - Page Not Found</h1>
+      </form>
+    )
+  }
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 import {BrowserRouter, Match, Miss} from 'react-router';
 // import the styles 
 import "./css/style.css";
@@ -12,6 +13,7 @@ const Root = () => {
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
       </div>
     </BrowserRouter>
   );
