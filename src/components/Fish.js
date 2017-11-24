4615cd3e67d6b935011104b15f035ed43a9d9024
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-24 18:57:29 +0200

    cleaned up the prop name by destructuring it into a const

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 class Fish extends React.Component {
   render() {
+    // destructure the fish details from the passed down state 
+    const {details} = this.props;
+    
     return (
       <div className="menu-fish">
-        {this.props.details.name}
+        {details.name}
       </div>
     )
   }
