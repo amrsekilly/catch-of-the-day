92ffcfa215728e51f0cd8c3303468a2b26466fec
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-25 00:34:44 +0200

    added the order functionality

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,6 +3,34 @@ import {formatPrice} from '../helpers';
 
 class Order extends React.Component {
 
+  // the constructor method
+  constructor() {
+    // to allow the use of this
+    super();
+    // bind the renderOrder method to the Order class
+    this.renderOrder = this.renderOrder.bind(this);
+  }
+
+  // render an item in the order 
+  renderOrder(key) {
+    // get the fish 
+    const fish = this.props.fishes[key];
+    // get the number of selected items of the fish
+    const count = this.props.order[key];
+
+    // if the fish is not available anymore, return a not available 
+    if(!fish || fish.status === 'unavailable') {
+      return <li key={key}>Sorry, we ran out of {fish ? fish.name : 'this type of fish'}</li>
+    }
+    // render the fish into the order 
+    return (
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  }
+
   render() {
     // get the order from the state
     const {order, fishes} = this.props;
@@ -28,6 +56,7 @@ class Order extends React.Component {
         <div className="order-wrap">
         <h2>Your Order</h2>
         <ul className="order">      
+        {orderKeys.map(this.renderOrder)}
           <li className="total">
             <strong>Total: {formatPrice(total)}</strong>
           </li>
