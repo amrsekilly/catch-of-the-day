1c4a0168a8b5e90dfe2e5fd1c49342cfad987712
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-24 19:02:37 +0200

    formatted the price into dollars

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// destructure the formatPrice from the hepers file
+import {formatPrice} from '../helpers';
 
 class Fish extends React.Component {
   render() {
@@ -10,7 +12,7 @@ class Fish extends React.Component {
         <img src={details.image} alt={details.name} />
         <h3 className="fish-name">
           {details.name}
-          <span className="price">{details.price}</span>
+          <span className="price">{formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
         <button>Add To Order</button>
