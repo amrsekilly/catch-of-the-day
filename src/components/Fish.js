c13e80853a2d88ceeb981182c40ceffb267ef480
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-24 18:54:11 +0200

    displayed the names of the fishes in the menu from the passed down state

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Fish extends React.Component {
   render() {
     return (
-      <div>
-        A fish element 🐡
+      <div className="menu-fish">
+        {this.props.details.name}
       </div>
     )
   }
