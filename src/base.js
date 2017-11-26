b0863b7ab72bdb58412a0b4e4401609b3a11e0ea
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-26 14:34:04 +0200

    added the firebase configs

--- src/base.js
+++ src/base.js
@@ -1,3 +1,10 @@
 // import the rebase package 
 const Base = require('re-base');
 
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBl76TYSFzk1mqpooKGeoOSrQJvNzIcYDY",
+  authDomain: "catch-of-the-day-amr.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-amr.firebaseio.com",
+});
+
+export default base;
